4534564d7e0b8e47e36687a948193eefc129d340
Administrator <3278602899>
2016-06-17 10:01:17 +0800

    fouth commit

--- MyApplication/xml/src/main/java/cn/justec/www/xml/MainActivity.java
+++ MyApplication/xml/src/main/java/cn/justec/www/xml/MainActivity.java
@@ -18,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //git1st
+        int test=222;
+        int git =test;
         dom();
     }
 
